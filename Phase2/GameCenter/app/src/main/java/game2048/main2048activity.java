01b9e27770eed5e2e4981a4a451df2c616734e9d
Winnie Xu <winnie.xu97@gmail.com>
2018-11-19 13:39:01 -0500

    first commit for 2048

---
+++ Phase2/GameCenter/app/src/main/java/game2048/main2048activity.java
@@ -0,0 +1,15 @@
+package game2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall2018.csc2017.slidingtiles.R;
+
+public class main2048activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2048activity);
+    }
+}
