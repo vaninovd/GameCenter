86a65b7da96b6d2d4e3751d762077dd1714f1ee9
Winnie Xu <winnie.xu97@gmail.com>
2018-11-19 13:50:48 -0500

    created the scorebox activity

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/Matrix2048.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/Matrix2048.java
@@ -3,7 +3,7 @@ package fall2018.csc2017.slidingtiles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Matrix extends AppCompatActivity {
+public class Matrix2048 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
