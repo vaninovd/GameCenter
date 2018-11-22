85a89335b7344351915a6ebfe3783576e41d9d7b
Dzhamaldin Vaninov <j.vaninov@mail.utoronto.ca>
2018-11-22 12:53:59 -0500

    I have added Java Doc to class TileFlipCard and added getters for variables front and back. Moreover, I have made the class TileFlipCard comparable and serializable.

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/TileFlipCard.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/TileFlipCard.java
@@ -1,6 +1,13 @@
 package fall2018.csc2017.slidingtiles;
 
-public class TileFlipCard {
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+
+/**
+ * A double sided tile in the Flip Card Game.
+ */
+public class TileFlipCard implements Comparable<TileFlipCard>, Serializable{
 
     /**
      * The unique id of every tile.
@@ -8,27 +15,34 @@ public class TileFlipCard {
     private int id;
 
     /**
-     * The id of the front side of the Tile. When the tile is first created the front side of the tile
-     * is blank.
+     * The id of the front side of the tile. When the tile is first created the front side of the
+     * tile is blank. This is the side of the tile that is shown to the user.
      */
     private int front = R.drawable.tile_def;;
 
     /**
-     * The id of the tile's picture, i.e. the image side of the tile. When the tile is first
-     * created the front side of the tile is blank.
+     * The id of the back side of the tile. When the tile is first created the back side of the
+     * tile is a picture. This is the side of the tile is hidden from the user.
      */
     private int back;
 
     /**
-     *
-     * @return
+     * The function is made to know whether the tile is facing up i.e. the picture side of the tile.
+     * @return true if tile is showing picture side or false if the tile is showing the blank side.
      */
     public boolean isUp() {
-        return isUp;
+        return tileUp;
     }
 
-    private boolean isUp = false;
+    /**
+     * A variable indicating wether the tile is up or down.
+     */
+    private boolean tileUp = false;
 
+    /**
+     * A tile with a tileId. Look up and set the id.
+     * @param tileId
+     */
     TileFlipCard(int tileId) {
         this.back = tileId;
         if (Board.NUM_COLS == 4) {
@@ -39,13 +53,20 @@ public class TileFlipCard {
         }
     }
 
-
+    /**
+     * Corresponding tiles for 4x4 board size
+     * @param tileId
+     */
     private void flipCard4x4(int tileId) {
         Object[] pics = {R.drawable.tile_1, R.drawable.tile_2, R.drawable.tile_3, R.drawable.tile_4,
                 R.drawable.tile_5, R.drawable.tile_6, R.drawable.tile_7, R.drawable.tile_8};
         id = (int) pics[tileId];
     }
 
+    /**
+     * Corresponding tiles for 6x6 board size.
+     * @param tileId
+     */
     private void flipCard6x6(int tileId) {
         Object[] pics = {R.drawable.tile_1, R.drawable.tile_2, R.drawable.tile_3, R.drawable.tile_4,
                 R.drawable.tile_5, R.drawable.tile_6, R.drawable.tile_7, R.drawable.tile_8,
@@ -55,21 +76,52 @@ public class TileFlipCard {
         id = (int) pics[tileId];
     }
 
+    /**
+     * Flips the tile up i.e. the user now sees the picture side of the tile.
+     */
     public void flipUp() {
         int tempId = this.front;
         this.front = this.back;
         this.back = tempId;
-        this.isUp = true;
+        this.tileUp = true;
     }
 
+    /**
+     * Flips down the tile i.e. the user sees the blank side of the tile.
+     */
     public void flipDown() {
         int temp = this.front;
         this.front = this.back;
         this.back = temp;
-        this.isUp = false;
+        this.tileUp = false;
     }
 
+    /**
+     * Refers to the unique id of the tile.
+     * @return the id of the tile.
+     */
     public int getId() {
         return this.id;
+    }
+
+    /**
+     * refers to the front of the tile i.e. whatever the user sees at the moment.
+     * @return front of the tile
+     */
+    public int getFront(){
+        return this.front;
+    }
+
+    /**
+     * Refers to the back of the tile i.e whatever is hidden from the user at the moment.
+     * @return the back of the tile
+     */
+    public int getBack(){
+        return this.back;
+    }
+
+    @Override
+    public int compareTo(@NonNull TileFlipCard o) {
+        return o.id - this.id;
     }
 }
